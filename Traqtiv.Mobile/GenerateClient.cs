ef0b3c44eba76bb583a7db42e4ffeb93f4000b18
GilmoS <gil.moshe23@gmail.com>
2026-05-11 18:26:20 +0300

    feat: add auto-generated SmartFitnessClient from NSwag
    - Generated from http://localhost:5203/swagger/v1/swagger.json
    - Contains all DTOs, Enums and API client methods

---
+++ Traqtiv.Mobile/GenerateClient.cs
@@ -0,0 +1,25 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+//this code is used to generate the SmartFitnessClient.cs file from the OpenAPI specification of the Smart Fitness API.
+//It uses the NSwag library to read the OpenAPI document and generate a C# client class that can be used to interact with the API.
+var document = await OpenApiDocument.FromUrlAsync("http://localhost:5203/swagger/v1/swagger.json");
+
+// Configure the C# client generator settings, including the class name and namespace for the generated code.
+var settings = new CSharpClientGeneratorSettings
+{
+    ClassName = "SmartFitnessClient",
+    CSharpGeneratorSettings =
+    {
+        Namespace = "Traqtiv.Mobile.Models"
+    }
+};
+
+// Create a new C# client generator using the OpenAPI document and the specified settings, then generate the client code.
+var generator = new CSharpClientGenerator(document, settings);
+var code = generator.GenerateFile();
+
+Directory.CreateDirectory("Models");
+await File.WriteAllTextAsync("Models/SmartFitnessClient.cs", code);
+
+Console.WriteLine("SmartFitnessClient.cs generated successfully!");
